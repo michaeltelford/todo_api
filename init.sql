b892578676ad46a51c6ec699493b539c55cfcc12
Michael Telford <michael.telford@live.com>
2020-02-21 18:01:20 +0000

    Added debug! dev dep

--- init.sql
+++ init.sql
@@ -7,24 +7,23 @@ CREATE TABLE todos (
   updated_on TIMESTAMP NOT NULL DEFAULT CURRENT_DATE
 );
 
-INSERT INTO todos (user_id, user_name, todos) VALUES
-  (
-    '19384673', 'Michael Telford',
-    '[
-  { "name": "Wash Dishes", "done": "true" },
-  { "name": "Do Laundry", "done": "true" },
-  { "name": "Write Code", "done": "false" },
-  { "name": "Watch Netflix", "done": "true" },
-  { "name": "Wash Car", "done": "false" }
-]'
-  ),
-  (
-    '19384665', 'Pablito Sanchez',
-    '[
-  { "name": "Clean Silverware", "done": "false" },
-  { "name": "Fix Clock", "done": "true" },
-  { "name": "Learn Rocket Science", "done": "false" },
-  { "name": "Watch Movie", "done": "true" },
-  { "name": "Wash Dog", "done": "true" }
-]'
-  );
+INSERT INTO todos (user_id, user_name, todos) VALUES (
+  '19384673', 'Michael Telford',
+  '[
+    { "name": "Wash Dishes", "done": "true" },
+    { "name": "Do Laundry", "done": "true" },
+    { "name": "Write Code", "done": "false" },
+    { "name": "Watch Netflix", "done": "true" },
+    { "name": "Wash Car", "done": "false" }
+  ]'
+),
+(
+  '19384665', 'Pablito Sanchez',
+  '[
+    { "name": "Clean Silverware", "done": "false" },
+    { "name": "Fix Clock", "done": "true" },
+    { "name": "Learn Rocket Science", "done": "false" },
+    { "name": "Watch Movie", "done": "true" },
+    { "name": "Wash Dog", "done": "true" }
+  ]'
+);
--- src/todo_api.cr
+++ src/todo_api.cr
@@ -13,6 +13,7 @@ end
 require "db"
 require "pg"
 require "kemal"
+require "debug"
 
 # Require local files.
 require "./todo_api/models/**"
