5ba740eb56b40a8e006ff88122ebb398ca088eae
Michael Telford <michael.telford@live.com>
2020-02-29 16:46:25 +0000

    Fixed CORS header typo

--- src/todo_api/controllers/common.cr
+++ src/todo_api/controllers/common.cr
@@ -5,8 +5,12 @@ end
 
 # CORS pre-flight requests.
 options "/*" do |env|
-  env.response.headers.add("Access-Control-Allow-Method", "*")
-  env.response.headers.add("Access-Control-Allow-Headers", "*")
+  env.response.headers.add("Access-Control-Allow-Methods", "POST,PUT,PATCH")
+  env.response.headers.add(
+    "Access-Control-Allow-Headers",
+    "Content-Type,If-Modified-Since,Cache-Control"
+  )
+  env.response.headers.add("Access-Control-Max-Age", "86400") # 24 hours.
 end
 
 # Healthcheck.
