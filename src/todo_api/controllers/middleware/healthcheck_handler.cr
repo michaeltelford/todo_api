5c35a89765a2bf76013489b97567f97de85c7105
Michael Telford <Michael.Telford@ibm.com>
2022-12-02 15:42:39 +0000

    Add /health path to healthcheck middleware

--- src/todo_api/controllers/middleware/healthcheck_handler.cr
+++ src/todo_api/controllers/middleware/healthcheck_handler.cr
@@ -2,7 +2,7 @@ class Middleware::HealthcheckHandler
   include HTTP::Handler
 
   def call(context)
-    if context.request.path == "/healthcheck"
+    if ["/health", "/healthcheck"].includes?(context.request.path)
       send_status(context, HTTP::Status::OK)
       return
     end
